0a7a79ee0bc681dadbe8b2fa095ead1a8329f32e
Wizard <rcvbuf@gmail.com>
2024-10-20 07:43:20 +0800

    fix

--- examples/basic/simple_loop_no_helpers.py
+++ examples/basic/simple_loop_no_helpers.py
@@ -12,7 +12,7 @@ def pretty_print_messages(messages):
     for message in messages:
         if message["content"] is None:
             continue
-        print(f"{message['sender']}: {message['content']}")
+        print(f"{message['role']}: {message['content']}")
 
 
 messages = []
